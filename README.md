2423338baffa772f38a89422ae1dd00ecc13e094
Leo Manfredi <leo.manfredi@gmail.com>
2024-04-02 16:10:49 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # telegraf-git-trees
 Git Trees for Telegraf Exec Input Plugin
+
+Count files of specific type, based on file extension, under a given path inside a GitHub repo.
+
+The script is used as command for [Telegraf Exec Input Plugin](https://github.com/influxdata/telegraf/tree/master/plugins/inputs/exec), e.g. `telegraf.conf`
+
+```conf
+[[inputs.exec]]
+  commands = ["/etc/telegraf/scripts/git_trees.py -o os-autoinst -r os-autoinst-distri-opensuse -p schedule/yam/ -t yaml -m qe_yam_schedule_yaml"]
+  timeout = "60s"
+  interval = "4h"
+  data_format = "influx"
+```
+
+For _standalone_ test, e.g.
+
+```python
+#!/usr/bin/env python3
+from git_trees import git_trees
+git_trees('os-autoinst', 'os-autoinst-distri-opensuse', 'schedule/yam/', 'yaml', 'qe_yam_schedule_yaml')
+```
---
+++ git_trees.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import argparse
+import requests
+
+def git_trees(owner, repo, path, type, measurement, branch='master') -> None:
+    """
+    See https://docs.github.com/en/rest/git/trees?apiVersion=2022-11-28
+    See https://docs.github.com/en/rest/using-the-rest-api/rate-limits-for-the-rest-api?apiVersion=2022-11-28
+    Note: The limit for unauthenticated requests is 60 requests per hour.
+    Note: The limit for the tree array is 100,000 entries with a maximum size of 7 MB when using the recursive parameter.
+    Print output as Line Protocol
+    See https://docs.influxdata.com/influxdb/cloud/reference/syntax/line-protocol/
+    """
+    api_url = f'https://api.github.com/repos/{owner}/{repo}/git/trees/{branch}?recursive=1'
+    r = requests.get(api_url)
+    r.raise_for_status()
+    json = r.json()
+    files = [ item["path"] for item in json["tree"] if item["path"].startswith(path) and item["path"].endswith(type)]
+    count = len(files)
+    print(f'{measurement},owner={owner},repo={repo},branch={branch},path={path} count={count}i')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--owner', required=True, help='The account owner of the repository. The name is not case sensitive')
+    parser.add_argument('-r', '--repo', required=True, help='The name of the repository without the .git extension. The name is not case sensitive.')
+    parser.add_argument('-p', '--path', required=True, help='The repo relative root path, e.g. schedule/yam/')
+    parser.add_argument('-t', '--type', required=True, help='The file type extension, e.g. yaml')
+    parser.add_argument('-m', '--measurement', required=True, help='The measurement name. Cannot begin with an underscore.')
+    parser.add_argument('-b', '--branch', required=False, default='master', help='The SHA1 value or ref (branch or tag) name of the tree. Default master.')
+    args = parser.parse_args()
+    git_trees(owner=args.owner, repo=args.repo, path=args.path, type=args.type, measurement=args.measurement, branch=args.branch)
